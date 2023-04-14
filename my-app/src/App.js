83c6878d2ad05ac4b4369fe442cd3c008705cca5
bahadirgltkn <bhdrgltknn@gmail.com>
2023-04-14 21:08:25 +0300

    instagram clone first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-full w-full flex items-center justify-center">
+      <div className="w-[380px] h-[581px] bg-logo-pattern"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "logo-pattern":
+          "url('https://static.cdninstagram.com/images/instagram/xig/homepage/phones/home-phones.png?__makehaste_cache_breaker=HOgRclNOosk')",
+      },
+    },
+  },
+  plugins: [],
+};
